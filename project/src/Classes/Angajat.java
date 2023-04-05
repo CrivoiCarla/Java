5023e2aa4c1e2c8805d956e7082fdc0da13859ca
CrivoiCarla <95349158+CrivoiCarla@users.noreply.github.com>
2023-04-05 15:00:49 +0300

    project

---
+++ project/src/Classes/Angajat.java
@@ -0,0 +1,64 @@
+package Classes;
+
+import java.time.LocalDate;
+
+public class Angajat extends Person
+{
+    private int angajatId;
+    private Rol rol;
+    private Sucursala sucursala;
+
+    public Angajat() { }
+
+    public Angajat(int angajatId, String firstName, String lastName, String email, Address address,
+                   LocalDate birthDate, Rol rol,  Sucursala sucursala)
+    {
+        super(firstName, lastName, email, address, birthDate);
+        this.angajatId = angajatId;
+        this.rol = rol;
+        this.sucursala = sucursala;
+    }
+
+    public int getAngajatId()
+    {
+        return angajatId;
+    }
+
+    public void setAngajatId(int angajatId)
+    {
+        this.angajatId = angajatId;
+    }
+
+    public Rol getRol()
+    {
+        return rol;
+    }
+
+    public void setRol(Rol rol)
+    {
+        this.rol = rol;
+    }
+
+
+    public Sucursala getSucursala()
+    {
+        return sucursala;
+    }
+
+    public void setSucursala(Sucursala sucursala)
+    {
+        this.sucursala = sucursala;
+    }
+
+    @Override
+    public String toString()
+    {
+        String s;
+        s = "\nAngajat ID: " + angajatId + "\nFirst name: " + firstName + "\nLast name: " + lastName +
+                "\nEmail: " + email + "\nAddress: " + address.toString() +
+                "\nBirth Date: " + birthDate.toString();
+        s = s + "\nRol: " + rol.getRol() + "\nSucursala: " + sucursala.getName() +"\n";
+
+        return s;
+    }
+}
---
+++ project/src/Classes/Sucursala.java
@@ -0,0 +1,41 @@
+package Classes;
+
+public class Sucursala
+{
+    private String name;
+    private Address address = new Address();
+
+    public Sucursala() { }
+
+    public Sucursala(String name, Address address)
+    {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public Address getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(Address address)
+    {
+        this.address = address;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Name: " + name + ", Address: " + address.toString();
+    }
+}
